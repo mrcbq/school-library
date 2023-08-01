842cba43d7c32a8540e317b5e73ff06703202c89
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-01 15:51:04 -0300

    Create method to hash student.

--- student.rb
+++ student.rb
@@ -17,4 +17,12 @@ class Student < Person
   def play_hooky
     '¯\\(ツ)/¯'
   end
+
+  def to_hash
+    {
+      name: @name,
+      age: @age,
+      parent_permission: @parent_permission,
+      classroom: @classroom
+    }
 end
