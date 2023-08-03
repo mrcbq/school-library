94753f78d83f8d351c7efcf1f1445f3160d553ba
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-03 16:19:36 -0300

    Add comments to fix linter errors.

--- capitalize_decorator.rb
+++ capitalize_decorator.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 require_relative 'decorator'
 
+# Represents a decorator that capitalizes names.
 class CapitalizeDecorator < Decorator
   def correct_name
     @nameable.correct_name.capitalize
