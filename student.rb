f05ede5d6697a9bdb9770a5e61660e8b4ea27ed8
Lincoln Gibson <lincolngibson7@gmail.com>
2023-08-03 16:21:17 -0300

    Add comments to fix linter errors and change quotes.

--- student.rb
+++ student.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
+# this is inherited from the person class
+
 require_relative 'classroom'
 require_relative 'person'
 
+# Represents a student.
 class Student < Person
   attr_reader :classroom
 
@@ -20,22 +25,22 @@ class Student < Person
 
   def to_hash
     super.merge(
-    {
-      class: 'Student',
-      name: @name,
-      age: @age,
-      parent_permission: @parent_permission,
-      classroom: @classroom
-    })
+      {
+        class: 'Student',
+        name: @name,
+        age: @age,
+        parent_permission: @parent_permission,
+        classroom: @classroom
+      }
+    )
   end
 
   def self.from_hash(hash)
-    id = hash["id"]
-    name = hash["name"]
-    age = hash["age"]
-    parent_permission = hash["parent_permission"]
-    classroom = hash["classroom"]
-    student = Student.new(id: id, name: name, age: age, parent_permission: parent_permission, classroom: classroom)
-    student
+    id = hash['id']
+    name = hash['name']
+    age = hash['age']
+    parent_permission = hash['parent_permission']
+    classroom = hash['classroom']
+    Student.new(id: id, name: name, age: age, parent_permission: parent_permission, classroom: classroom)
   end
 end
