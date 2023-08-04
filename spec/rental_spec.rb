0347cf9f81dbfef0d86b96507157433586a2e262
mrcbq <marcobaquero@gmail.com>
2023-08-03 19:06:03 -0500

    End test for from_hash method at the rental class, Correct Linters Errors

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../rental'
 require_relative '../book'
 require_relative '../student'
@@ -7,17 +9,19 @@ RSpec.describe Rental do
   describe '#initialize' do
     it 'creates a new rental with valid attributes' do
       book = Book.new(title: 'Sample Book', author: 'John Doe')
-      student = Student.new(id: 12345, classroom: 'Maths', name: 'Richard', age: 45)
+      student = Student.new(id: 12_345, classroom: 'Maths', name: 'Richard', age: 45)
       rental = Rental.new(book: book, person: student, date: '23/8/23')
       expect(rental.book.title).to eq('Sample Book')
       expect(rental.person.name).to eq('Richard')
     end
   end
+end
 
+RSpec.describe Rental do
   describe '#to_hash' do
     it 'returns a hash representation of the rental' do
       book = Book.new(title: 'Sample Book', author: 'John Doe')
-      student = Student.new(id: 12345, classroom: 'Maths', name: 'Richard', age: 45)
+      student = Student.new(id: 12_345, classroom: 'Maths', name: 'Richard', age: 45)
       rental = Rental.new(book: book, person: student, date: '23/8/23')
       expected_hash = {
         date: '23/8/23',
@@ -26,8 +30,8 @@ RSpec.describe Rental do
           author: 'John Doe'
         },
         person: {
-          id: 12345,
-          class: "Student",
+          id: 12_345,
+          class: 'Student',
           name: 'Richard',
           age: 45,
           parent_permission: true,
@@ -37,22 +41,24 @@ RSpec.describe Rental do
       expect(rental.to_hash).to eq(expected_hash)
     end
   end
+end
 
+RSpec.describe Rental do
   describe '.from_hash' do
     it 'creates a new rental from a hash' do
       rental_hash = {
-      'date' => '23/8/23',
-      'book' => {
-        'title' => 'Sample Book',
-        'author' => 'John Doe'
-      },
-      'person' => {
-        'id' => 12345,
-        'name' => 'Richard',
-        'age' => 45,
-        'parent_permission' => true
+        'date' => '23/8/23',
+        'book' => {
+          'title' => 'Sample Book',
+          'author' => 'John Doe'
+        },
+        'person' => {
+          'id' => 12_345,
+          'name' => 'Richard',
+          'age' => 45,
+          'parent_permission' => true
+        }
       }
-    }
       rental = Rental.from_hash(rental_hash)
       expect(rental.date).to eq('23/8/23')
       expect(rental.book.title).to eq('Sample Book')
@@ -60,4 +66,3 @@ RSpec.describe Rental do
     end
   end
 end
-
